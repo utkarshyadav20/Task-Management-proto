23a105bef7c6efc0d0db683ef94995351d90ee87
RunawayGOD <20022002utkarsh@gmail.com>
2022-09-01 23:08:45 +0530

    updated the js

--- scripts/index.js
+++ scripts/index.js
@@ -31,3 +31,28 @@ const taskContent=document.querySelector(".task_content");
 //after saving the info inside our DOM we also need to access it we  do this with the help of taskModal variable we craeted
 const taskModal=document.querySelector(".task_modal_body");
 console.log(taskContent);
+//making a function to add html in the DOM
+const taskTaskContent=({id,url,title,description,type})=>{`
+<div class="col-md-6 col-lg-4 mt-3 " id=${id} key=${id}>
+<div class="card shadow-sm task_card">
+  <div class="card-header d-flex gap-2 justify-content-end task_card_header">
+       <button type="button" class="btn btn-outline-info mr-2" name=${id}>
+        <i class="fas fa-pencil-alt" name=${id}></i>
+       </button>
+       <button type="button" class="btn btn-outline-danger mr-2" name=${id}>
+        <i class="fas fa-trash-alt" name=${id}></i>
+       </button>
+  </div>
+  <div class="card-body">
+    ${
+      url && <img></img>
+    }
+  </div>
+</div>
+</div>
+`
+
+};
+
+
+
