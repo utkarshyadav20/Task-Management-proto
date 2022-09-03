00a98b6cee1276a98c70ac65c8e8bd8d6edd0aeb
RunawayGOD <20022002utkarsh@gmail.com>
2022-09-03 12:27:58 +0530

    updated

--- scripts/index.js
+++ scripts/index.js
@@ -34,22 +34,34 @@ console.log(taskContent);
 //making a function to add html in the DOM
 const taskTaskContent=({id,url,title,description,type})=>{`
 <div class="col-md-6 col-lg-4 mt-3 " id=${id} key=${id}>
-<div class="card shadow-sm task_card">
-  <div class="card-header d-flex gap-2 justify-content-end task_card_header">
-       <button type="button" class="btn btn-outline-info mr-2" name=${id}>
-        <i class="fas fa-pencil-alt" name=${id}></i>
-       </button>
-       <button type="button" class="btn btn-outline-danger mr-2" name=${id}>
-        <i class="fas fa-trash-alt" name=${id}></i>
-       </button>
+  <div class="card shadow-sm task_card">
+    <div class="card-header d-flex gap-2 justify-content-end task_card_header">
+         <button type="button" class="btn btn-outline-info mr-2" name=${id}>
+          <i class="fas fa-pencil-alt" name=${id}></i>
+         </button>
+         <button type="button" class="btn btn-outline-danger mr-2" name=${id}>
+          <i class="fas fa-trash-alt" name=${id}></i>
+         </button>
+    </div>
+    <div class="card-body">
+          ${
+            url && `<img width="100%" src=${url} alt="card_image_cap" class="card-image-top md-3 rounded-lg">`
+          }
+          <h4 class="class_card_title">${title}</h4>
+          <p class="description trim-3-lines text-muted" data-gram_editor="false">
+            ${description}
+          </p>
+          <div class="tags text-white d-flex flex-wrap">
+            <span class="badge bg-success m-1">
+              ${type}
+            </span>
+      </div>
+    </div>
+    <div class="card-footer">
+       <button type="button" class="btn btn-outline-primary float-end" data-bs-toggle="modal" data-bs-target="#ShowTasks">Open Task</button>
+    </div>
   </div>
-  <div class="card-body">
-    ${
-      url && <img></img>
-    }
   </div>
-</div>
-</div>
 `
 
 };
